935a8a8ca9e7227047f753fe30483c36149e4dc6
Matthew Hopson <mhopson@hotmail.co.uk>
2024-02-21 22:25:52 +0000

    Add camera gui

--- src/Graphics/Camera.cpp
+++ src/Graphics/Camera.cpp
@@ -1,9 +1,12 @@
 #include "Camera.h"
 
+#include <imgui.h>
+
 PerspectiveCamera::PerspectiveCamera(unsigned width, unsigned height, float fov)
+    : aspect_{static_cast<float>(width) / static_cast<float>(height)}
+    , fov_{fov}
 {
-    auto aspect = static_cast<float>(width) / static_cast<float>(height);
-    projection_matrix_ = glm::perspective(glm::radians(fov), aspect, 0.25f, 3000.0f);
+    projection_matrix_ = glm::perspective(glm::radians(fov_), aspect_, near_, far_);
 }
 
 void PerspectiveCamera::update()
@@ -11,6 +14,25 @@ void PerspectiveCamera::update()
     forwards_ = forward_vector(transform.rotation);
     glm::vec3 centre = transform.position + glm::normalize(forwards_);
     view_matrix_ = glm::lookAt(transform.position, centre, {0, 1, 0});
+}
+
+void PerspectiveCamera::gui()
+{
+    if (ImGui::Begin("Camera")) {
+        if (ImGui::SliderFloat("FOV", &fov_, 1.0f, 179.0f))
+        {
+            projection_matrix_ = glm::perspective(glm::radians(fov_), aspect_, near_, far_);
+        }
+        if (ImGui::SliderFloat("Near", &near_, 1.0f, 10.0f))
+        {
+            projection_matrix_ = glm::perspective(glm::radians(fov_), aspect_, near_, far_);
+        }
+        if (ImGui::SliderFloat("Far", &far_, 100.0f, 10000.0f))
+        {
+            projection_matrix_ = glm::perspective(glm::radians(fov_), aspect_, near_, far_);
+        }
+    }
+    ImGui::End();
 }
 
 const glm::mat4& PerspectiveCamera::get_view_matrix() const
@@ -27,3 +49,4 @@ const glm::vec3& PerspectiveCamera::get_forwards() const
 {
     return forwards_;
 }
+
--- src/Graphics/Camera.h
+++ src/Graphics/Camera.h
@@ -10,6 +10,7 @@ struct PerspectiveCamera
   public:
     PerspectiveCamera(unsigned window_size, unsigned window_width, float fov);
     void update();
+    void gui();
 
     const glm::mat4& get_view_matrix() const;
     const glm::mat4& get_projection() const;
@@ -19,4 +20,9 @@ struct PerspectiveCamera
     glm::mat4 projection_matrix_{1.0f};
     glm::mat4 view_matrix_{1.0f};
     glm::vec3 forwards_{0.0f};
+
+    float aspect_ = 0;
+    float fov_ = 0;
+    float near_ = 1.0f;
+    float far_ = 20000.0f; 
 };
